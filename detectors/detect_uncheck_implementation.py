687d5497431f0c368a06ffaf51570ca86ca6079e
wzy <ziyue0530@gmail.com>
2025-06-23 18:05:57 +0800

    更新detector

---
+++ detectors/detect_uncheck_implementation.py
@@ -0,0 +1,29 @@
+import os
+
+
+# 调用gigahorse，执行自定义的datalog文件，判断是否存在MissingImplementationValidation.csv文件
+def detect_uncheck_implementation(file_path: str) -> bool:
+    # 构造命令，执行自定义factory.dl文件
+    command = (
+        f"../gigahorse-toolchain/gigahorse.py "
+        f"--timeout_secs=300 "
+        f"-C ../datalog/uncheck_implementation.dl "
+        f"{file_path}"
+    )
+    # 执行gigahourse
+    os.system(command)
+    # 判断out文件夹是否输出IsFactory文件
+    contract_name = os.path.splitext(os.path.basename(file_path))[0]
+    is_factory_file = os.path.join(".", ".temp", contract_name, "out", "MissingImplementationValidation.csv")
+    if os.path.exists(is_factory_file) and os.path.getsize(is_factory_file) > 0:
+        # 如果不是工厂，gigahorse会创建一个空的IsFactory.csv文件
+        print(f"INFO: Found non-empty MissingImplementationValidation.csv. Contract '{contract_name}' has the issue.")
+        return True
+
+    print(f"INFO: IsFactory.csv not found or is empty. Contract '{contract_name}' is safe.")
+    return False
+
+
+if __name__ == '__main__':
+    file_path = "../tests/factory/ERC1155SeaDropCloneFactory.hex"
+    detect_uncheck_implementation(file_path)
